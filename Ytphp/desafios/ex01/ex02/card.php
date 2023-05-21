98d2c6be6ad544aaf23c80c533ac579ab33688a2
dudurb17 <eduardo.robettibedin@gmail.com>
2023-05-21 13:30:21 -0300

    criando segundo exemplo do exercicio 1

---
+++ Ytphp/desafios/ex01/ex02/card.php
@@ -0,0 +1,32 @@
+<?php
+$num = $_GET['num']?? 0;
+
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css" />
+    <link href="/your_path_to_version_6_compiled_files/css/fontawesome.css" rel="stylesheet">
+    <title>Desafios PHP</title>
+</head>
+
+<body>
+    <main>
+        <h1><b>Resultado Final</b></h1>
+        <?php
+        echo '<p>O número escolhido foi <b>' . $num . '</b></p>';
+        echo '<p>O seu antecessor é <b>' . $num-1 . '</b></>';
+        echo '<p>O seu sucessor é <b>' . $num+1  . '</b></p>';
+        ?>
+        <button onclick="history.go(-1);">
+            &larr; Voltar
+        </button>
+    </main>
+</body>
+
+</html>
