ce5f12256784a008cbc04de7088247637ddce9c0
dudurb17 <eduardo.robettibedin@gmail.com>
2023-05-21 21:34:57 -0300

    fazendo um convertor de real para dolar com API

--- Ytphp/desafios/ex04/ex01/card.php
+++ Ytphp/desafios/ex04/ex01/card.php
@@ -1,10 +1,15 @@
-
-Ainda em teste
-
 <?php
-$valor = $_GET['valor'];
 
-$ch = curl_init("https://olinda.bcb.gov.br/olinda/servico/PTAX/versao/v1/odata/CotacaoDolarDia(dataCotacao=@dataCotacao)?@dataCotacao='08-14-2020'&format=json");
+        $valor = $_GET['valor']?? 0;
+        
+        // link bcd.gov.br
+        $inicio= date('m-d-Y', strtotime("-7 days")); //pegando a data de sete dias atrás
+        $fim=date("m-d-Y");
+        $url='https://olinda.bcb.gov.br/olinda/servico/PTAX/versao/v1/odata/CotacaoDolarPeriodo(dataInicial=@dataInicial,dataFinalCotacao=@dataFinalCotacao)?@dataInicial=\''.$inicio.'\'&@dataFinalCotacao=\''.$fim.'\'&$top=1&$orderby=dataHoraCotacao%20desc&$format=json&$select=cotacaoCompra,dataHoraCotacao';//vou mudar a data, a data estacva um valor fixo e vou colocar minha variaves
+        $dados=json_decode(file_get_contents($url), true); // true é para deixar em valor de array e se colocar em false coloca em valor de objeto;
+        $cotação= $dados['value'][0]['cotacaoCompra'];
+        $money = $valor/$cotação ;
+
 
 ?>
 
@@ -15,17 +20,21 @@ $ch = curl_init("https://olinda.bcb.gov.br/olinda/servico/PTAX/versao/v1/odata/C
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="style.css" />
     <title>Document</title>
 </head>
 
 <body>
     <main>
-        <h1><b>Conversor de Moedas v2.0</b></h1>
-        <?php echo "<p>Seus R$ " . number_format($valor, 2, ',', '.') .
-            " </p> equivalem <b>" . $ch;
-        ?>
+        <h1>Conversor de Moedas v1.0</h1>
+        <p>Seu R$ <?php echo number_format($valor, 2, ",", ".") .
+                        "  equivalem a US$ " . number_format($money, 2, ',', '.'); ?></p>
+        <?php echo "<b>Cotação do dia de R$" . number_format( $cotação, 2, ",", ".") . "</b> informada diretamente no codigo" ?>
+        <button onclick="window.location.href = 'index.html'">Voltar</button>
+
+
     </main>
+
 </body>
 
 </html>
