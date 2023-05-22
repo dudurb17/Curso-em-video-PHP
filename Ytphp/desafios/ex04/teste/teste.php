418f0b257aab3d7e794a5fdb3060f2b551ee0787
dudurb17 <eduardo.robettibedin@gmail.com>
2023-05-21 21:27:06 -0300

    exemplo de API

--- Ytphp/desafios/ex04/teste/teste.php
+++ Ytphp/desafios/ex04/teste/teste.php
@@ -1,7 +1,9 @@
 <pre><?php 
     // link bcd.gov.br
-    $url='https://olinda.bcb.gov.br/olinda/servico/PTAX/versao/v1/odata/CotacaoDolarPeriodo(dataInicial=@dataInicial,dataFinalCotacao=@dataFinalCotacao)?@dataInicial=\'05-14-2023\'&@dataFinalCotacao=\'05-21-2023\'&$top=1&$orderby=dataHoraCotacao%20desc&$format=json&$select=cotacaoCompra,dataHoraCotacao';
-    $dados=json_decode(file_get_contents($url), true);
+    $inicio= date('m-d-Y', strtotime("-7 days")); //pegando a data de sete dias atrás
+    $fim=date("m-d-Y");
+    $url='https://olinda.bcb.gov.br/olinda/servico/PTAX/versao/v1/odata/CotacaoDolarPeriodo(dataInicial=@dataInicial,dataFinalCotacao=@dataFinalCotacao)?@dataInicial=\''.$inicio.'\'&@dataFinalCotacao=\''.$fim.'\'&$top=1&$orderby=dataHoraCotacao%20desc&$format=json&$select=cotacaoCompra,dataHoraCotacao';//vou mudar a data, a data estacva um valor fixo e vou colocar minha variaves
+    $dados=json_decode(file_get_contents($url), true); // true é para deixar em valor de array e se colocar em false coloca em valor de objeto;
     var_dump($dados);
 
     $cotação= $dados['value'][0]['cotacaoCompra'];
